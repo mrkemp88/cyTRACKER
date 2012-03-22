9f0f5897d212f195ef6d68e31dd61171330bc1d3
Ray <mrkemp88@me.com>
2012-03-21 20:10:56 -0700

    Finished with layout and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def new
+    @title = "New User"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tracker::Application.routes.draw do
+  resources :users
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   
