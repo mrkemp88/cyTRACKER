763087e826abe9317bba3c4899ddd0c7887d23cb
Ray <mrkemp88@me.com>
2012-03-21 22:48:13 -0700

    User creation successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
   end
   
   def new
+  	@user = User.new
     @title = "New User"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+    	flash[:success] = "User created successfully."
+      redirect_to @user
+    else
+      @title = "New User"
+      render 'new'
+    end
   end
 
 end
