ba2cfd52f885786f8ed93c827c97c0ed8dd98c61
Ray <mrkemp88@me.com>
2012-03-23 03:57:26 -0700

    New user field tests and cleaner flash code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       @title = "New User"
+      @user.password = ""
+      @user.password_confirmation = ""
       render 'new'
     end
   end
