81994dcc12c151f24e8f4c26cd121b7db1ae0700
Ray <mrkemp88@me.com>
2012-03-24 01:03:19 -0400

    Nav bar on every page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @title = @user.name
+    @sidebar = "Navigation stuff goes here..."
   end
   
   def new
